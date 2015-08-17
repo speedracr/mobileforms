d3653bf509ff378c0cb48ec24aa180787e5a24ae
Jonas Knipper <jonas@robocomobo.com>
2015-08-17 21:41:25 +0200

    Set up slick slides

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-$('.form-rtl').slick({
-  rtl: true
-});
-  console.log("Loaded!");
+  $('.slide-wrapper').slick({
+     arrows: false,
+     infinite: false,
+     cssEase: 'none'
+  });
+  $('.slick-prev').on('click', function(){
+     $('.slide-wrapper').slick('slickPrev');
+     console.log('hello');
+  })
+  $('.slick-next').on('click', function(){
+     $('.slide-wrapper').slick('slickNext');
+     console.log('hello');
+  })
 });
